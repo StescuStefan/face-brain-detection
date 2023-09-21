7f6a78edd56e184fba0722dbf5bcc9a6688c21a6
Stescu Ștefan <stescustefan01@gmail.com>
2023-09-21 16:40:46 +0300

    Add files via upload

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Rank = ({ name, entries }) => {
+  return (
+    <div>
+      <div className="white f3">
+        {`${name}, your current entry count is ${entries}`}
+      </div>
+      <div className="white f1"></div>
+    </div>
+  );
+};
+
+export default Rank;
